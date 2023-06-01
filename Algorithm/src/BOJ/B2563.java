d4b7a10e176e6423da9acb0e7966283129791428
ws1811 <rnsjtm0811@gmail.com>
2023-06-01 23:54:09 +0900

    백준 2563 - 색종이

--- Algorithm/src/BOJ/B2563.java
+++ Algorithm/src/BOJ/B2563.java
@@ -10,30 +10,26 @@ public class B2563 {
 	 두 번째 자연수는 색종이의 아래쪽 변과 도화지의 아래쪽 변 사이의 거리이다. 
 	 색종이의 수는 100 이하이며, 색종이가 도화지 밖으로 나가는 경우는 없다
 	 */
+	
+	
 	public static void main(String[] args) {
-		int[][] white = new int[99][99]; // 가로, 세로의 크기가 각각 100인 정사각형 모양의 흰색 도화지 - 초기값 null('\0')
-		
-		//입력
+		int[][] arr = new int[100][100];
+		int area = 0;
 		Scanner scan = new Scanner(System.in);
-		int num = scan.nextInt(); // 검은 색종이의 개수
-		int[][] black = new int[99][2]; // 검은 색종이들을 붙인 위치를 저장할 배열(최대100개)
-		// 색종이를 붙인 위치 - 정사각형의 좌측 하단 꼭지점의 위치로 주어진다 (X, Y)
-		//num 개 만큼 색종이를 붙인 위치 입력받기	
-		for(int i=0; i<num;i++) {					
-			for(int j=0;j<2;j++) {
-				black[i][j] = scan.nextInt();
+		int num = scan.nextInt(); // 색종이의 개수
+		for(int i=0; i<num; i++) {
+			int x = scan.nextInt();
+			int y = scan.nextInt();
+			for(int a=x; a<x+10; a++) {
+				for(int b=y; b<y+10;b++) {
+					if(arr[a][b] != 1) {
+						arr[a][b] = 1;
+						area++;
+					}
+				}
 			}
 		}
-		
-		// black 배열에서 시작점별로 양 변 길이가 10인 정사각형 해당하는 부분의 값을 1로 변경(white배열에서)
-		for(int i=0; i<num; i++) {
-			int x = black[i][0]; //x좌표
-			int y = black[i][1]; //y좌표
-			int 
-		}
-		
-		
-
+		System.out.println(area);
 	}
 
 }
